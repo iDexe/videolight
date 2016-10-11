d8bdf552a8ebdb78ee2e3690113229f0d56462dc
iDexe <nils@nilss-mac-mini.speedport_w_723v_1_40_000>
2016-10-11 16:27:36 +0200

    menu 0.1
    submenu added

--- VideoLight1.3/VideoLight1.3.ino
+++ VideoLight1.3/VideoLight1.3.ino
@@ -3,7 +3,6 @@
 
 #include <U8glib.h>
 #include <PinChangeInterrupt.h>
-#include <avr/sleep.h>
 #include <Wire.h>
 #include <Encoder.h>
 
@@ -62,6 +61,7 @@ class Screen
     void setBattery(int Bat, bool def);
     void setBottom(String Bottom, bool def);
     void setCaption(String Caption, bool def);
+    void setMenu(char *menu[], int point, bool def);
     void update();
     void renderChanges();
   private:
@@ -74,6 +74,9 @@ class Screen
     bool _showBottom;
     String _Caption;
     bool _showCaption;
+    char **_menu;
+    bool _showMenu;
+    int _menuPoint;
 };
 Encoder knob(DT, CLK);
 class RotEnc
@@ -132,10 +135,15 @@ void setup() {
   buttonOn.begin(true);
 
 }
+enum mode {VIDEO, INSANE, PORTION, HALOGEN, STROBE, MORSE, FLICKER, TORCH, MENU};
+mode Mode = VIDEO;
+
 enum prog_state {STANDBY, ON};
 prog_state Prog = ON;
+
 enum state {TMP, INT, SUPER_INT};
 state State = TMP;
+
 bool superint = false;
 bool stateChange = true;
 bool progChange = false;
@@ -143,97 +151,284 @@ long Tmp = 5000;
 long Int = 50;
 long Super_Int = 50;
 
+enum video_state {videoTMP, videoINT};
+video_state videoState = videoINT;
+int videoTmp = 5000;
+int videoInt = 50;
 
+enum insane_state {insaneTMP, insaneINT};
+insane_state insaneState = insaneINT;
+int insaneTmp = 5000;
+int insaneInt = 50;
+
+enum portion_state {portionAMOUNT, portionTIME};
+portion_state portionState;
+
+enum halogen_state {halogenINT};
+halogen_state halogenState;
+
+enum strobe_state {strobeTIME1, strobeTIME2};
+strobe_state strobeState;
+
+enum morse_state {morsePUSH};
+morse_state morseState;
+
+enum flicker_state {flickerAMOUNT, flickerINT, flickerCOLOR};
+flicker_state flickerState;
+
+enum torch_state {torchAMOUNT, torchINT, torchCOLOR};
+torch_state torchState;
 
 void loop() {
   switch (Prog) {
     case ON: {
-        switch (State) {
-          case TMP: {//Farbtemperatur einstellen
-              if (stateChange) {//Beim ersten ausführen
-                screen.setBigText(String(Tmp) + "K", true);
-                screen.setCaption("Temperatur", true);
-                screen.update();
-                rotenc.setPos(-10, 10, (Tmp - 5000) / 100);
-                stateChange = false;
-              }
+        switch (Mode) {
+          case MENU:
+          if (stateChange) {//only on the first time
+            
+            screen.setBigText("",false);
+            screen.setCaption("Menu", false);
+            char *menu[] = {"VIDEO","INSANE","PORTION","HALOGEN"};
+            screen.setMenu(menu, 1, true);
+            screen.update();
+            stateChange = false;
+            
+          }
 
-              if (rotenc.check(-10, 10)) { //Wenn jemand dreht
-                Tmp = rotenc.getPos() * 100 + 5000;
-                screen.setBigText(String(Tmp) + "K", true);
-                light.color((Tmp - 4000) / 100 * 255 / 20);
-              }
 
-              if (button1.pressed()) {//Wenn jemand drückt
-                if (superint) {
-                  State = SUPER_INT;
-                } else {
-                  State = INT;
+            break;
+          case VIDEO:
+            switch (videoState) {
+              case videoTMP: //Adjust color temperature
+                if (stateChange) {//Only on the first time
+                  screen.setBigText(String(videoTmp) + "K", true);
+                  screen.setCaption("VIDEO: Temperature", true);
+                  screen.update();
+                  rotenc.setPos(-10, 10, (videoTmp - 5000) / 100);
+                  stateChange = false;
                 }
 
-                stateChange = true;
-              }
-              break;
+                if (rotenc.check(-10, 10)) { //When somebody turns
+                  videoTmp = rotenc.getPos() * 100 + 5000;
+                  screen.setBigText(String(videoTmp) + "K", true);
+                  light.color((videoTmp - 4000) / 100 * 255 / 20);
+                }
+
+                if (button1.pressed()) {//When somebody presses
+                  videoState = videoINT;
+                  stateChange = true;
+                }
+                break;
+              case videoINT:
+                if (stateChange) {//Only on the first time
+                  screen.setBigText(String(Int) + "%", true);
+                  screen.setCaption("VIDEO: Brightness", true);
+                  screen.update();
+                  rotenc.setPos(-10, 10, (Int - 50) / 5);
+                  stateChange = false;
+                }
+
+                if (rotenc.check(-10, 10)) { //When somebody turns
+                  Int = rotenc.getPos() * 5 + 50;
+                  screen.setBigText(String(Int) + "%", true);
+                  light.brightness(Int / 5 * 255 / 20);
+                }
+
+                if (button1.pressed()) {//When seomebody presses
+                  videoState = videoTMP;
+                  stateChange = true;
+                }
+
+                break;
             }
-          case INT: {//Helligkeit einstellen
-              if (stateChange) {//Beim ersten ausführen
-                screen.setBigText(String(Int) + "%", true);
-                screen.setCaption("Helligkeit", true);
-                screen.update();
-                rotenc.setPos(-10, 10, (Int - 50) / 5);
-                stateChange = false;
-              }
 
-              if (rotenc.check(-10, 10)) { //Wenn jemand dreht
-                Int = rotenc.getPos() * 5 + 50;
-                screen.setBigText(String(Int) + "%", true);
-                light.brightness(Int / 5 * 255 / 20);
-              }
+            break;
+          case INSANE:
+            switch (insaneState) {
+              case insaneTMP:
+                if (stateChange) {//Beim ersten ausführen
+                  screen.setBigText(String(insaneTmp) + "K", true);
+                  screen.setCaption("INSANE: Temperature", true);
+                  screen.update();
+                  rotenc.setPos(-10, 10, (insaneTmp - 5000) / 100);
+                  stateChange = false;
+                }
 
-              if (button1.pressed()) {//Wenn jemand drückt
-                State = TMP;
-                stateChange = true;
-              }
-              if (button1.hold(1000)) {
-                State = SUPER_INT;
-                superint = true;
-                stateChange = true;
-              }
-              break;
+                if (rotenc.check(-10, 10)) { //Wenn jemand dreht
+                  insaneTmp = rotenc.getPos() * 100 + 5000;
+                  screen.setBigText(String(insaneTmp) + "K", true);
+                  light.color((insaneTmp - 4000) / 100 * 255 / 20);
+                }
+
+                if (button1.pressed()) {//Wenn jemand drückt
+                  insaneState = insaneINT;
+                  stateChange = true;
+                }
+
+                break;
+              case insaneINT:
+                if (stateChange) {//Beim ersten ausführen
+                  screen.setBigText(String(insaneInt) + "%", true);
+                  screen.setCaption("INSANE: Brightness", true);
+                  screen.update();
+                  rotenc.setPos(-10, 10, (insaneInt - 50) / 5);
+                  stateChange = false;
+                }
+
+                if (rotenc.check(-10, 10)) { //Wenn jemand dreht
+                  insaneInt = rotenc.getPos() * 5 + 50;
+                  screen.setBigText(String(insaneInt) + "%", true);
+                  light.insane(insaneInt / 5 * 255 / 20);
+                }
+
+                if (button1.pressed()) {//Wenn jemand drückt
+                  insaneState = insaneTMP;
+                  stateChange = true;
+                }
+
+                break;
             }
-          case SUPER_INT: {
-              if (stateChange) {//Beim ersten ausführen
-                screen.setBigText(String(Super_Int) + "%", true);
-                screen.setCaption("Insane", true);
-                screen.update();
-                rotenc.setPos(-10, 10, (Super_Int - 50) / 5);
-                stateChange = false;
-              }
-
-              if (rotenc.check(-10, 10)) { //Wenn jemand dreht
-                Super_Int = rotenc.getPos() * 5 + 50;
-                screen.setBigText(String(Super_Int) + "%", true);
-                light.insane(Super_Int / 5 * 255 / 20);
-              }
-
-              if (button1.pressed()) {//Wenn jemand drückt
-                State = TMP;
-                stateChange = true;
-              }
-              if (button1.hold(1000)) {
-                State = INT;
-                superint = false;
-                stateChange = true;
-              }
-              break;
+            break;
+          case PORTION:
+            switch (portionState) {
+              case portionAMOUNT:
+                break;
+              case portionTIME:
+                break;
             }
+            break;
+          case HALOGEN:
+            switch (halogenState) {
+              case halogenINT:
+                break;
+            }
+            break;
+          case STROBE:
+            switch (strobeState) {
+              case strobeTIME1:
+                break;
+              case strobeTIME2:
+                break;
+            }
+            break;
+          case MORSE:
+            switch (morseState) {
+              case morsePUSH:
+                break;
+            }
+            break;
+          case FLICKER:
+            switch (flickerState) {
+              case flickerAMOUNT:
+                break;
+              case flickerINT:
+                break;
+              case flickerCOLOR:
+                break;
+            }
+            break;
+          case TORCH:
+            switch (torchState) {
+              case torchAMOUNT:
+                break;
+              case torchINT:
+                break;
+              case torchCOLOR:
+                break;
+            }
+            break;
         }
+
+
+
+        /*
+                switch (State) {
+                  case TMP: {//Farbtemperatur einstellen
+                      if (stateChange) {//Beim ersten ausführen
+                        screen.setBigText(String(Tmp) + "K", true);
+                        screen.setCaption("Temperatur", true);
+                        screen.update();
+                        rotenc.setPos(-10, 10, (Tmp - 5000) / 100);
+                        stateChange = false;
+                      }
+
+                      if (rotenc.check(-10, 10)) { //Wenn jemand dreht
+                        Tmp = rotenc.getPos() * 100 + 5000;
+                        screen.setBigText(String(Tmp) + "K", true);
+                        light.color((Tmp - 4000) / 100 * 255 / 20);
+                      }
+
+                      if (button1.pressed()) {//Wenn jemand drückt
+                        if (superint) {
+                          State = SUPER_INT;
+                        } else {
+                          State = INT;
+                        }
+
+                        stateChange = true;
+                      }
+                      break;
+                    }
+                  case INT: {//Helligkeit einstellen
+                      if (stateChange) {//Beim ersten ausführen
+                        screen.setBigText(String(Int) + "%", true);
+                        screen.setCaption("Helligkeit", true);
+                        screen.update();
+                        rotenc.setPos(-10, 10, (Int - 50) / 5);
+                        stateChange = false;
+                      }
+
+                      if (rotenc.check(-10, 10)) { //Wenn jemand dreht
+                        Int = rotenc.getPos() * 5 + 50;
+                        screen.setBigText(String(Int) + "%", true);
+                        light.brightness(Int / 5 * 255 / 20);
+                      }
+
+                      if (button1.pressed()) {//Wenn jemand drückt
+                        State = TMP;
+                        stateChange = true;
+                      }
+                      if (button1.hold(1000)) {
+                        State = SUPER_INT;
+                        superint = true;
+                        stateChange = true;
+                      }
+                      break;
+                    }
+                  case SUPER_INT: {
+                      if (stateChange) {//Beim ersten ausführen
+                        screen.setBigText(String(Super_Int) + "%", true);
+                        screen.setCaption("Insane", true);
+                        screen.update();
+                        rotenc.setPos(-10, 10, (Super_Int - 50) / 5);
+                        stateChange = false;
+                      }
+
+                      if (rotenc.check(-10, 10)) { //Wenn jemand dreht
+                        Super_Int = rotenc.getPos() * 5 + 50;
+                        screen.setBigText(String(Super_Int) + "%", true);
+                        light.insane(Super_Int / 5 * 255 / 20);
+                      }
+
+                      if (button1.pressed()) {//Wenn jemand drückt
+                        State = TMP;
+                        stateChange = true;
+                      }
+                      if (button1.hold(1000)) {
+                        State = INT;
+                        superint = false;
+                        stateChange = true;
+                      }
+                      break;
+                    }
+                }
+        */
 
         //Allways running Code, like screen rendering, temperature and battery monitoring.
 
         //2 Temp sensor configuration
         char voltstr[5];
-        dtostrf(volt1.read(1000) * 16.8 / 810,4,1,voltstr);
+        dtostrf(volt1.read(1000) * 16.8 / 810, 4, 1, voltstr);
         screen.setBottom("LED:" + String(int(therm1.read(1000))) + (char)176 + "C BAT:" + String(int(therm2.read(1000))) + (char)176 + "C " + String(voltstr) + "V", true);
 
         //3 Temp sensor configuration
@@ -242,23 +437,9 @@ void loop() {
         screen.renderChanges();
 
         if (buttonOn.pressed() && millis() > 1000) {
-          //Prog = STANDBY;
-          //progChange = true;
-          if (superint) {
-            superint = false;
-            State = INT;
-          } else {
-            superint = true;
-            State = SUPER_INT;
-          }
+          Mode = MENU;
           stateChange = true;
         }
-        /*
-        if (buttonOn.hold(1000)) {
-          Prog = STANDBY;
-          progChange = true;
-        }
-        */
         break;
       }
     case STANDBY: {
@@ -276,8 +457,6 @@ void loop() {
             Prog = ON;
             stateChange = true;
           }
-          //delay(1000);
-          //sleepNow();
         }
       }
   }
@@ -363,17 +542,17 @@ void Light::brightness(int Int) {
 void Light::insane(int Int) {
   //Brightness between 0 (dark) and 510 (very bright)
   _superInt = Int;
-  int warm = 2* _superInt * _color / 255;
+  int warm = 2 * _superInt * _color / 255;
   if (warm > 255) {
     warm = 255;
   }
-  int cold = 2* _superInt * (255 - _color) / 255;
+  int cold = 2 * _superInt * (255 - _color) / 255;
   if (cold > 255) {
     cold = 255;
   }
   analogWrite(_warm, warm);
   analogWrite(_cold, cold);
-  
+
 }
 
 //Screen-methods
@@ -439,6 +618,18 @@ void Screen::setCaption(String Caption, bool def) {
   }
 }
 
+void Screen::setMenu(char *menu[], int point, bool def) {
+  _showMenu = def;
+  if (point != _menuPoint) {
+    _menuPoint = point;
+    _valueChange = true;
+  }
+  if (menu != _menu) {
+    _menu = menu;
+    _valueChange = true;
+  }
+}
+
 void Screen::update() {
   _valueChange = true;
 }
@@ -476,6 +667,32 @@ void Screen::renderChanges() {
         u8g.setPrintPos((128 - u8g.getStrWidth(Caption)) / 2, 0);
         u8g.print(_Caption);
       }
+
+      //Menu
+      if (_showMenu) {
+        u8g.setFont(u8g_font_helvB08);
+        u8g.setFontPosTop();
+        /*
+        u8g.setPrintPos(5,0);
+        u8g.print("Hallo1");
+        u8g.setPrintPos(5,u8g.getFontAscent());
+        u8g.print("Hallo2");
+        */
+        char *menu1[] = {"VIDEO","INSANE","PORTION","HALOGEN","STROBE","MORSE","FLICKER","TORCH"};
+        for (int i=0; i < 10; i++) {
+          if (i = 1) {
+            u8g.drawBox(64*(i/5),(i%5)*(u8g.getFontAscent()+2),u8g.getStrWidth(menu1[i]),u8g.getFontDescent());
+            
+            
+            u8g.setColorIndex(1);
+          }else{
+            u8g.setColorIndex(0);
+          }
+          u8g.setPrintPos(64*(i/5),(i%5)*(u8g.getFontAscent()+2));
+          u8g.print(menu1[i]);
+        }
+      }
+      
 
     } while ( u8g.nextPage() );
     reset();
@@ -560,23 +777,6 @@ double Voltage::read(int interval) {
     return _Volt;
   }
   return _Volt;
-}
-
-
-
-void sleepNow() {
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-  sleep_enable();
-  //attachInterrupt(0,wakeUpNow,LOW);
-  attachPinChangeInterrupt(digitalPinToPCINT(BUTTON_ON), wakeUpNow, FALLING);
-  sleep_mode();
-  sleep_disable();
-  //detachInterrupt(0);
-  detachPinChangeInterrupt(digitalPinToPCINT(BUTTON_ON));
-}
-
-void wakeUpNow() {
-
 }
 
 
